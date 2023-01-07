d6417745b13502eb78c22aed32a2b4d83f2f8280
raiyuvraj <yuvirai017@gmail.com>
2023-01-08 03:16:04 +0530

    [Yuvraj]-Harmonic Number

---
+++ BasicCorePrograming/HarmonicNmber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograming
+{
+    internal class HarmonicNmber
+    {
+        public void Harmonic()
+        {
+            Console.WriteLine("Find the Harmonic Numbers");
+            Console.WriteLine("Enter Harmonic Value which we have to find");
+            int HarmonicNum = Convert.ToInt32(Console.ReadLine());
+            double result = 1.0;
+            Console.WriteLine("1/1");
+
+            for (int i = 2; i <= HarmonicNum; i++)
+            {
+                Console.WriteLine($"+1/{i}");
+                result += 1 / (double)i;
+
+            }
+            Console.WriteLine("nth Harmonic value = " + result);
+        }
+    }
+}
--- BasicCorePrograming/Program.cs
+++ BasicCorePrograming/Program.cs
@@ -8,7 +8,7 @@
             while(flag)
             {
                 Console.WriteLine("Welcome to Basic Core Programs");
-                Console.WriteLine("1: Flip Coin" + "\n" + "2: Leap Year" + "\n" +  "3: Power of 2" + "\n"+"4: Exit");
+                Console.WriteLine("1: Flip Coin" + "\n" + "2: Leap Year" + "\n" +  "3: Power of 2" + "\n"+ "4: Harmonic Number" +"\n"+"5: Exit");
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch(check)
                 {
@@ -23,6 +23,10 @@
                         case 3:
                             PowerOf2 power = new PowerOf2();
                         power.Power();
+                        break;
+                        case 4:
+                            HarmonicNmber harmonic = new HarmonicNmber();
+                            harmonic.Harmonic();
                         break;
                         default: break;
                         flag= false;
